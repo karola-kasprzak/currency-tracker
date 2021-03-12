ad4f4fe394d42a5f833ba18a109e7d89bd581444
karola-kasprzak <karola.a.kasprzak@gmail.com>
2021-03-12 13:54:30 +0100

    project ini

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, h1 } from "react-bootstrap";
+
+function App() {
+    return (
+        <Container>
+            <h1>NBP Currency Rates</h1>
+        </Container>
+    );
+}
+
+export default App;
