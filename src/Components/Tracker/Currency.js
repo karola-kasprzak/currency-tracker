5d30fe3b610b1f5caaa8e3c64b130f35fa745c26
karola-kasprzak <karola.a.kasprzak@gmail.com>
2021-03-13 22:41:06 +0100

    buttons added

--- src/Components/Tracker/Currency.js
+++ src/Components/Tracker/Currency.js
@@ -63,15 +63,25 @@ export default class Currency extends Component {
 
         const currencyDisplayList = currencyRates.map((item, index) => {
             return (
-                <div
-                    className="w-75 d-flex justify-content-between"
-                    key={index}
-                >
+                <div className="d-flex justify-content-between" key={index}>
                     <span className="pr-3">{item.code}</span>
                     <span className="pr-3">{item.currency}</span>
                     <span> {item.mid}</span>
                     <Button>
-                        <i class="bi-alarm"></i>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-trash"
+                            viewBox="0 0 16 16"
+                        >
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                            <path
+                                fill-rule="evenodd"
+                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                            />
+                        </svg>
                     </Button>
                 </div>
             );
