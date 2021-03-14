d67b85477dbfb6b578077dd50c9a9752ab59a09b
karola-kasprzak <karola.a.kasprzak@gmail.com>
2021-03-14 13:17:40 +0100

    adding and removing currencies logic added

--- src/Components/Tracker/Currency.js
+++ src/Components/Tracker/Currency.js
@@ -12,23 +12,55 @@ export default class Currency extends Component {
             effectiveDate: "",
             // currencyList: [],
             userCurrencies: [],
+            selectedCurrency: null,
         };
     }
 
-    addToUserCurrencies = (item) => {
+    addToUserCurrencies = () => {
         let newUserCurrencies = this.state.userCurrencies;
-        newUserCurrencies.push(item);
+        let selectedCurrency = this.state.currencyRates.find(
+            (element) => element.code === this.state.selectedCurrency
+        );
+        //currency rate is pushed to array if it is not undefined and not already present in the array
+        if (
+            selectedCurrency !== undefined &&
+            newUserCurrencies.findIndex(
+                (element) => element.code === this.state.selectedCurrency
+            ) < 0
+        ) {
+            newUserCurrencies.push(selectedCurrency);
+        }
+
         this.setState({
             userCurrencies: newUserCurrencies,
         });
-        console.log("added!");
+        console.log(newUserCurrencies);
     };
 
     clearUserCurrencies = () => {
         this.setState({
             userCurrencies: [],
         });
-        console.log("cleared!");
+        console.log("all cleared!");
+    };
+
+    removeUserCurrency = (e) => {
+        let removeCurrencyCode = e.target.getAttribute("code");
+        console.log(removeCurrencyCode);
+        let newUserCurrencies = this.state.userCurrencies.filter(
+            (element) => element.code !== removeCurrencyCode
+        );
+
+        this.setState({
+            userCurrencies: newUserCurrencies,
+        });
+        console.log(newUserCurrencies);
+    };
+
+    handleSelect = (e) => {
+        let selectValue = e.target.value;
+        this.setState({ selectedCurrency: selectValue });
+        console.log(`Selected ${selectValue}`);
     };
 
     componentDidMount() {
@@ -43,7 +75,7 @@ export default class Currency extends Component {
     }
 
     render() {
-        const { currencyRates, effectiveDate } = this.state;
+        const { currencyRates, userCurrencies, effectiveDate } = this.state;
 
         // console.log(effectiveDate, currencyRates);
 
@@ -61,27 +93,14 @@ export default class Currency extends Component {
             );
         });
 
-        const currencyDisplayList = currencyRates.map((item, index) => {
+        const currencyDisplayList = userCurrencies.map((item, index) => {
             return (
                 <div className="d-flex justify-content-between" key={index}>
                     <span className="pr-3">{item.code}</span>
                     <span className="pr-3">{item.currency}</span>
                     <span> {item.mid}</span>
-                    <Button>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            fill="currentColor"
-                            class="bi bi-trash"
-                            viewBox="0 0 16 16"
-                        >
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                            <path
-                                fill-rule="evenodd"
-                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-                            />
-                        </svg>
+                    <Button code={item.code} onClick={this.removeUserCurrency}>
+                        x
                     </Button>
                 </div>
             );
@@ -94,7 +113,12 @@ export default class Currency extends Component {
                     <label htmlFor="currencySelect">
                         Add a currency to track:
                     </label>
-                    <select className="form-control" id="currencySelect">
+                    <select
+                        className="form-control"
+                        id="currencySelect"
+                        onChange={this.handleSelect}
+                    >
+                        <option value="null">---</option>
                         {currencySelectOptions}
                     </select>
                     <Button onClick={this.addToUserCurrencies}>
