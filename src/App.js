3ccf6260afcf922a4c2238613568471498e9e7b9
karola-kasprzak <karola.a.kasprzak@gmail.com>
2021-03-14 20:43:44 +0100

    custom css added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import Tracker from "./Components/Tracker/Tracker";
+import "./App.css";
 
 function App() {
     return (
-        <Container className="bg-light py-4 my-4">
-            <h1 className="text-center">NBP Currency Rates Tracker</h1>
-            <Tracker />
-        </Container>
+        <div className="py-4 custom-bg">
+            <Container className="custom-container py-4 mb-4 rounded">
+                <h1 className="text-center">NBP Currency Rates Tracker</h1>
+                <Tracker />
+            </Container>
+        </div>
     );
 }
 
